44178301f21f223a0f4189103affd2413751fbdb
Dan Feder <dafeder@gmail.com>
2018-05-27 13:27:21 -0400

    Recreate ahoy docker commands

--- src/Commands/DockerCommands.php
+++ src/Commands/DockerCommands.php
@@ -9,18 +9,88 @@ namespace DkanTools\Commands;
 class DockerCommands extends \Robo\Tasks
 {
     /**
-     * Bring up docker containers.
+     * Bring up docker containers for project.
      */
     function dockerUp()
     {
-        $confMain = __DIR__ . '/../../assets/docker/docker-compose.common.yml';
-        $confVolume = __DIR__ . '/../../assets/docker/docker-compose.nosync.yml';
-        $confProxy = __DIR__ . '/../../assets/docker/docker-compose.noproxy.yml';
+        $dockerUpStack = $this->getDockerStack('up -d');
+        $dockerUpStack->run();
+    }
 
-        $dockerCommandBase = "docker-compose -f $confMain -f $confVolume -f $confProxy up -d";
+    /**
+     * Bring down docker containers for project.
+     */
+    function dockerStop()
+    {
+        $dockerStopStack = $this->getDockerStack('stop');
+        $dockerStopStack->run();
+    }
 
-        $result = $this->taskExec($dockerCommandBase)
+    /**
+     * Docker exec command --
+     */
+    function dockerExec(string $cmd, string $service = 'cli')
+    {
+        $dockerComposeStack = $this->getDockerStack("exec $service $cmd");
+        $dockerComposeStack->run();
+    }
+
+    /**
+     * Arbitrary docker compose command --
+     */
+    function dockerCompose(string $cmd)
+    {
+        $dockerComposeStack = $this->getDockerStack($cmd);
+        $dockerComposeStack->run();
+    }
+
+    /**
+     * Get a taskExecStack object that forms the base of all docker-compose
+     * commands.
+     *
+     * @param string Docker compose action to append (e.g. "stop").
+     * @return object A CollectionBuilder object built with taskExecStack(),
+     *                ready to run().
+     */
+    function getDockerStack($cmd)
+    {
+        $confMain = dirname(__DIR__, 2) . '/assets/docker/docker-compose.common.yml';
+        $confVolume = dirname(__DIR__, 2) . '/assets/docker/docker-compose.nosync.yml';
+        $confProxy = dirname(__DIR__, 2) . '/assets/docker/docker-compose.noproxy.yml';
+
+        $dockerCommand = "docker-compose -f $confMain -f $confVolume -f $confProxy $cmd";
+        $slug = str_replace(['-', '_'], '', basename(getcwd()));
+
+        return $this->taskExecStack()
+            ->exec("export SLUG=$slug")
+            ->exec('export DKTL_DIRECTORY=' . __DIR__)
+            ->exec('export DKTL_CURRENT_DIRECTORY=' . getcwd())
+            ->exec('export PROXY_DOMAIN='. $this->getDockerProxy())
+            ->exec($dockerCommand);
+    }
+
+    /**
+     * Helper function to get the proxy domain for docker compose.
+     *
+     * @return string Domain for proxy.
+     */
+    function getDockerProxy()
+    {
+        $inspect = 'docker inspect proxy 2> /dev/null | grep docker.domain | tr -d \' ",-\' | cut -d \= -f 2 | head -1';
+        $res = $this->taskExec($inspect)
+            ->printOutput(FALSE)
             ->run();
-        $this->say($result->getMessage());
+        // Check for proxy container, get domain from that.
+        if ($proxy_domain = $res->getMessage()) {
+            return $proxy_domain;
+        }
+        // If no proxy is running, use the overridden or default proxy domain
+        elseif ($proxy_domain = getenv('AHOY_WEB_DOMAIN')) {
+            return $proxy_domain;
+        }
+        else {
+            return 'localtest.me';
+        }
+
     }
 }
