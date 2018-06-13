bed7c13dd61fa196cfef3d6f1585eacbe46ff896
Dan Feder <dafeder@gmail.com>
2018-06-13 10:49:39 -0400

    Better document docker-compose commands.

--- src/Commands/DockerCommands.php
+++ src/Commands/DockerCommands.php
@@ -13,7 +13,20 @@ class DockerCommands extends \Robo\Tasks
     const DKTL_DOCKER_PHP_ERROR = "This command was run in error; docker commands should run through dktl.sh";
 
     /**
-     * Run a docker-compose command. E.g. dktl docker:compose ps. Alias  "dc".
+     * Run a docker-compose command. E.g. "dktl docker:compose ps".
+     *
+     * Run a docker-compose command, with configuration options already added.
+     *
+     * Examples:
+     *
+     * * "dktl docker:compose ps" to list project's containers and their state
+     * * "dktl docker:compose up -d" to start up docker containers
+     * * "dktl docker:compose exec cli bash" for a command prompt in the cli
+     * * "dktl docker:compose stop" to shut down a project's containers
+     * * "dktl docker:compose rm" to erase a project's containers
+     *
+     * You may also run a shorter alias, "dc" (e.g. "dktl dc ps"). See
+     * https://docs.docker.com/compose/reference/ for a full list of commands.
      */
      function dockerCompose()
      {
