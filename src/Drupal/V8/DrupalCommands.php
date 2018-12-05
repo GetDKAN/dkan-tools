31389e6255ec7b7386f73f870ecb65d73b4eabeb
fmizzell <fmizzell@1312210.no-reply.drupal.org>
2018-12-05 00:13:46 -0600

    More stuff.

--- src/Drupal/V8/DrupalCommands.php
+++ src/Drupal/V8/DrupalCommands.php
@@ -13,12 +13,92 @@ class DrupalCommands extends \Robo\Tasks
     /**
      * Run make for Drupal core.
      */
+    public function drupalGet($version)
+    {
+        Util::prepareTmp();
+
+        $archive = $this->getDrupalArchive($version);
+        $task = $this->taskExec("tar -xvzf {$archive}")->dir(Util::TMP_DIR);
+        $task->run();
+
+        // At this point we should have the unbuilt Drupal folder in tmp.
+        $this->drupalTempReplace(str_replace(".tar.gz", "", $archive));
+        Util::cleanupTmp();
+    }
+
+    private function getDrupalArchive($version)
+    {
+        $fileName = "drupal-{$version}";
+        $archive = Util::TMP_DIR . "/{$fileName}";
+        if (file_exists($archive)) {
+            $this->io()->warning("Drupal archive $fileName.tar.gz already exists; skipping download, will attempt extraction.");
+            return $archive;
+        }
+
+        $sources = [
+            "https://ftp.drupal.org/files/projects/{$fileName}.tar.gz",
+        ];
+
+        $source = null;
+        foreach ($sources as $s) {
+            if (Util::urlExists($s)) {
+                $source = $s;
+                break;
+            }
+        }
+
+        if (!isset($source)) {
+            $this->io()->error("No archive available for Drupal $version.");
+            return;
+        }
+
+        $this->io()->section("Getting Drupal from {$source}");
+        $this->taskExec("wget -O {$archive} {$source}")->run();
+        return $archive;
+    }
+
+    private function drupalTempReplace($tmp_drupal)
+    {
+        $drupal_permanent = Util::getProjectDirectory() . '/docroot';
+        $replaced = false;
+        if (file_exists($drupal_permanent)) {
+            if ($this->io()->confirm("Are you sure you want to replace your current DKAN profile directory?")) {
+                $this->_deleteDir($drupal_permanent);
+                $replaced = true;
+            } else {
+                $this->say('Canceled.');
+                return;
+            }
+        }
+        $this->_exec('mv ' . $tmp_drupal . ' ' . $drupal_permanent);
+        $verb = $replaced ? 'replaced' : 'created';
+        $this->say("Drupal directory $verb.");
+    }
+
+    /**
+     * Run make for Drupal core.
+     */
     public function drupalMake($opts = ['yes|y' => false])
     {
+        $this->mergeComposerConfig();
+        $docroot = Util::getProjectDirectory() . "/docroot";
+        $this->_exec("composer --working-dir={$docroot} update");
         $this->linkSitesDefault();
         $this->linkModules();
         $this->linkThemes();
+        $this->linkInterra();
+        $this->linkJsonForm();
+    }
 
+    private function mergeComposerConfig() {
+        $drupal_config_path = Util::getProjectDirectory() . "/docroot/composer.json";
+        $drupal_config = json_decode(file_get_contents($drupal_config_path));
+        $custom_path = Util::getProjectDirectory() . "/src/make/composer.json";
+
+        $include_array = $drupal_config->extra->{"merge-plugin"}->include;
+        $include_array[] = $custom_path;
+        $drupal_config->extra->{"merge-plugin"}->include = $include_array;
+        file_put_contents($drupal_config_path, json_encode($drupal_config, JSON_PRETTY_PRINT));
     }
 
 
@@ -58,7 +138,7 @@ class DrupalCommands extends \Robo\Tasks
             exit;
         }
 
-        $result = $this->_exec('ln -s ../../../../src/modules docroot/modules/custom');
+        $result = $this->_exec('ln -s ../../src/modules docroot/modules/custom');
         if ($result->getExitCode() != 0) {
             $this->io()->error('Could not crete link');
             return $result;
@@ -75,12 +155,52 @@ class DrupalCommands extends \Robo\Tasks
             throw new \Exception("Could not link themes. Folders 'src/themes' and 'docroot' must both be present to create link.");
             return;
         }
-        $result = $this->_exec('ln -s ../../../../src/themes docroot/themes/custom');
+        $result = $this->_exec('ln -s ../../src/themes docroot/themes/custom');
         if ($result->getExitCode() != 0) {
             $this->io()->error('Could not crete link');
             return $result;
         }
 
         $this->io()->success('Successfully linked src/themes to docroot/sites/all/themes/custom');
+    }
+
+    /**
+     * Link src/themes to  docroot/sites/all/modules/themes.
+     */
+    private function linkInterra()
+    {
+        $result = $this->_exec('ln -s profiles/contrib/dkan2/modules/custom/interra_frontend/interra docroot/interra');
+        if ($result->getExitCode() != 0) {
+            $this->io()->error('Could not crete link');
+            return $result;
+        }
+
+        $this->io()->success('Successfull');
+    }
+
+    /**
+     * Link src/themes to  docroot/sites/all/modules/themes.
+     */
+    private function linkJsonForm()
+    {
+        $result = $this->_exec('ln -s vendor/bower-asset docroot/libraries');
+        if ($result->getExitCode() != 0) {
+            $this->io()->error('Could not crete link');
+            return $result;
+        }
+
+        $this->io()->success('Successfull');
+    }
+
+    /**
+     * Remove all gitignores from docroot.
+     */
+    public function drupalRemoveGitIgnores() {
+        $gitignores = [];
+        exec("find docroot -type f -name '.gitignore'", $gitignores);
+
+        foreach ($gitignores as $gitignore) {
+            `rm {$gitignore}`;
+        }
     }
 }
