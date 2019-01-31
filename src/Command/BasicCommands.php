0297e59801f72189096bccd395ca6d021e1eced6
Dan Feder <dafeder@gmail.com>
2019-01-31 12:02:11 -0500

    XDEBUG_DKTL check and documentation.

--- src/Command/BasicCommands.php
+++ src/Command/BasicCommands.php
@@ -102,8 +102,32 @@ class BasicCommands extends \Robo\Tasks
         }
     }
 
+    /**
+     * Check for existence of XDEBUG_DKTL environment variable.
+     */
+    private function xdebugCheck() {
+        $xdebugDktl = getenv("XDEBUG_DKTL");
+        if (!$xdebugDktl) {
+            throw new \Exception("XDEBUG_DKTL environment variable must be "
+            . " set to use this command.");
+        }
+    }
+
+    /** 
+     * Start xdebug on CLI and web containers.
+     * 
+     * This command adds a .ini file to your src/docker/etc/php directory and
+     * restarts the CLI and web containers. It checks for compatibility by
+     * looking for a DKTL_XDEBUG environment variable. This is set by default in
+     * DKAN Tools' containers, but you may need to set it again if using your
+     * own.
+     * 
+     * Adding /src/docker/etc to your project .gitignore is reccomended.
+     */
     public function xdebugStart() 
     {
+        $this->xdebugCheck();
+        
         $platform = getenv("PLATFORM");
         $sourceFile = ($platform == 'Darwin') ? 'xdebug-macos.ini' : 'xdebug-linux.ini';
         $dktlRoot = Util::getDktlDirectory();
@@ -121,8 +145,16 @@ class BasicCommands extends \Robo\Tasks
         Util::directoryAndFileCreationCheck($result, $f, $this->io());
     }
 
+    /** 
+     * Stop xdebug on CLI and web containers.
+     * 
+     * Removes the xdebug.ini file and restarts CLI and web containers. See 
+     * xdebug:start for more information.
+     */
     public function xdebugStop()
     {
+        $this->xdebugCheck();
+
         $f = 'src/docker/etc/php/xdebug.ini';
         $result = unlink($f);
         if ($result) {
