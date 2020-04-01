0c5a68f9770b8229a047b2fc1525709491b2c463
Thierry Dallacroce <thierry.dallacroce@semanticbits.com>
2020-03-31 17:25:51 -0700

    Switch array notation, tweak indentation in Settings.docker.php

--- assets/d8/site/settings.docker.php
+++ assets/d8/site/settings.docker.php
@@ -1,17 +1,18 @@
 <?php
+
 if (getenv("DKTL_DOCKER") == "1") {
-    $databases = array(
-    'default' => array(
-      'default' => array(
-        'database' => 'drupal',
-        'username' => 'drupal',
-        'password' => '123',
-        'host' => 'db',
-        'port' => '',
-        'driver' => 'mysql',
-        'prefix' => '',
-      ),
-    ),
-    );
+    $databases = [
+        'default' => [
+            'default' => [
+                'database' => 'drupal',
+                'username' => 'drupal',
+                'password' => '123',
+                'host' => 'db',
+                'port' => '',
+                'driver' => 'mysql',
+                'prefix' => '',
+            ],
+        ],
+    ];
     $settings['file_public_base_url'] = "http://dkan/sites/default/files";
 }
