01ccba39f2a81d35729286bae9302a39ae38c871
Riadh Habbachi <rhabbachi@users.noreply.github.com>
2020-05-14 05:47:00 +0100

    fix: make deploy script work with dkan2 instances (#213)

--- assets/script/deploy.sh
+++ assets/script/deploy.sh
@@ -14,11 +14,11 @@ fi
 DOCROOT=$1
 ENV=$2
 
-drush --root=$DOCROOT rr
-drush --root=$DOCROOT cc all
-drush --root=$DOCROOT en environment -y
-drush --root=$DOCROOT en environment_indicator -y
-drush --root=$DOCROOT en dkan_environment -y
-drush --root=$DOCROOT env-switch $ENV --force
-drush --root=$DOCROOT updb -y
-[ -f $DOCROOT/../src/script/deploy.custom.sh ] && $DOCROOT/../src/script/deploy.custom.sh $DOCROOT $ENV || echo "No custom deployment script."
+drush --root="$DOCROOT" cr
+drush --root="$DOCROOT" updb -y
+
+if [ -f "$DOCROOT"/../src/script/deploy.custom.sh ]; then
+  "$DOCROOT"/../src/script/deploy.custom.sh "$DOCROOT" "$ENV"
+else
+  echo "No custom deployment script."
+fi
