ed491151714d7278ba930d6dd016e4d617e63eb2
Janette Day <janette.day@civicactions.com>
2022-05-06 11:34:14 -0500

    improvements

--- src/Command/ProjectCommands.php
+++ src/Command/ProjectCommands.php
@@ -10,94 +10,99 @@ use DkanTools\Util\TestUserTrait;
  *
  * @see http://robo.li/
  */
-class ProjectCommands extends Tasks
-{
-    use TestUserTrait;
+class ProjectCommands extends Tasks {
+  use TestUserTrait;
 
-    /**
-     * Run project cypress tests.
-     */
-    public function projectTestCypress(array $args)
-    {
+  /**
+   * Run project cypress tests.
+   */
+  public function projectTestCypress(array $args) {
 
-        $this->createTestUsers();
+    $this->createTestUsers();
 
-        $result = $this->taskExec("npm link ../../../../usr/local/bin/node_modules/cypress")
-            ->dir("src/tests")
-            ->run();
-        if ($result->getExitCode() != 0) {
-            $this->io()->error('Could not symlink package folder');
-            return $result;
-        }
-
-        $task = $this
-            ->taskExec('npm install --force')
-            ->dir("src/tests");
-        $result = $task->run();
-        if ($result->getExitCode() != 0) {
-            $this->io()->error('Could not insall test dependencies.');
-            return $result;
-        }
-        $this->io()->success('Installation of test dependencies successful.');
-        $config = file_exists("src/tests/cypress.json") ? '--config-file src/tests/cypress.json' : '';
-        $cypress = $this->taskExec('CYPRESS_baseUrl="http://$DKTL_PROXY_DOMAIN" npx cypress run' . $config)
-            ->dir("src/tests");
-
-        foreach ($args as $arg) {
-            $cypress->arg($arg);
-        }
-
-        $cypress->run();
-        return $this->deleteTestUsers();
+    $result = $this->taskExec("npm link ../../../../usr/local/bin/node_modules/cypress")
+      ->dir("src/frontend")
+      ->run();
+    if ($result && $result->getExitCode() === 0) {
+      $this->io()->success(
+        'Successfully symlinked global cypress into frontend folder.'
+      );
+    }
+    else {
+      $this->io()->error('Could not symlink package folder');
+      return $result;
     }
 
-    /**
-     * Run Site PhpUnit Tests. Additional phpunit CLI options can be passed.
-     *
-     * @see   https://phpunit.de/manual/6.5/en/textui.html#textui.clioptions
-     * @param array $args
-     *   Arguments to append to phpunit command.
-     */
-    public function projectTestPhpunit(array $args)
-    {
-        $proj_dir = Util::getProjectDirectory();
-        $phpunit_executable = $this->getPhpUnitExecutable();
+    $task = $this
+      ->taskExec('npm install --force')
+      ->dir("src/tests");
+    $result = $task->run();
+    if ($result->getExitCode() != 0) {
+      $this->io()->error('Could not insall test dependencies.');
+      return $result;
+    }
+    $this->io()->success('Installation of test dependencies successful.');
+    $config = file_exists("src/tests/cypress.json") ? ' --config-file src/tests/cypress.json' : '';
+    $cypress = $this->taskExec('CYPRESS_baseUrl="http://$DKTL_PROXY_DOMAIN" npx cypress run' . $config)
+      ->dir("src/tests");
 
-        $phpunitExec = $this->taskExec($phpunit_executable)
-            ->option('testsuite', 'Custom Test Suite')
-            ->dir("{$proj_dir}/docroot/modules/custom");
-
-        foreach ($args as $arg) {
-            $phpunitExec->arg($arg);
-        }
-
-        return $phpunitExec->run();
+    foreach ($args as $arg) {
+      $cypress->arg($arg);
     }
 
-    /**
-     *
-     */
-    private function getPhpUnitExecutable()
-    {
-        $proj_dir = Util::getProjectDirectory();
+    $cypress->run();
+    return $this->deleteTestUsers();
+  }
 
-        $phpunit_executable = $phpunit_executable = "{$proj_dir}/vendor/bin/phpunit";
+  /**
+   * Run Site PhpUnit Tests. Additional phpunit CLI options can be passed.
+   *
+   * @param array $args
+   *   Arguments to append to phpunit command.
+   *
+   * @see https://phpunit.de/manual/6.5/en/textui.html#textui.clioptions
+   */
+  public function projectTestPhpunit(array $args) {
+    $proj_dir = Util::getProjectDirectory();
+    $phpunit_executable = $this->getPhpUnitExecutable();
 
-        if (!file_exists($phpunit_executable)) {
-            $this->taskExec("dktl installphpunit")->run();
-            $phpunit_executable = "phpunit";
-        }
+    $phpunitExec = $this->taskExec($phpunit_executable)
+      ->option('testsuite', 'Custom Test Suite')
+      ->dir("{$proj_dir}/docroot/modules/custom");
 
-        return $phpunit_executable;
+    foreach ($args as $arg) {
+      $phpunitExec->arg($arg);
     }
 
-    /**
-     *
-     */
-    private function inGitDetachedState($dkanDirPath)
-    {
-        $output = [];
-        exec("cd {$dkanDirPath} && git rev-parse --abbrev-ref HEAD", $output);
-        return (isset($output[0]) && $output[0] == 'HEAD');
+    return $phpunitExec->run();
+  }
+
+  /**
+   * Determine path to PHPUnit executable.
+   */
+  private function getPhpUnitExecutable() {
+    $proj_dir = Util::getProjectDirectory();
+
+    $phpunit_executable = $phpunit_executable = "{$proj_dir}/vendor/bin/phpunit";
+
+    if (!file_exists($phpunit_executable)) {
+      $this->taskExec("dktl installphpunit")->run();
+      $phpunit_executable = "phpunit";
     }
+
+    return $phpunit_executable;
+  }
+
+  /**
+   * Ensure current git branch is not in a detached state.
+   *
+   * @return bool
+   *   Flag for whether the current branch branch is detached.
+   */
+  private function inGitDetachedState($dkanDirPath) {
+    $output = [];
+    exec("cd {$dkanDirPath} && git rev-parse --abbrev-ref HEAD", $output);
+    return (isset($output[0]) && $output[0] == 'HEAD');
+  }
+
 }
--- src/Util/TestUserTrait.php
+++ src/Util/TestUserTrait.php
@@ -5,67 +5,63 @@ namespace DkanTools\Util;
 /**
  * Test User Trait.
  */
-trait TestUserTrait
-{
+trait TestUserTrait {
 
-    /**
-     * Private create user.
-     */
-    private function createTestUsers()
-    {
-        $people = $this->getUsers();
-        foreach ($people as $person) {
-            $name = $person->name;
-            $mail = $person->mail;
-            $role = $person->role;
-            $this->taskExecStack()
-                ->stopOnFail()
-                ->exec("dktl drush user:create $name --password=$name --mail=$mail")
-                ->exec("dktl drush user-add-role $role $name")
-                ->run();
-        }
+  /**
+   * Private create user.
+   */
+  private function createTestUsers() {
+    $people = $this->getUsers();
+    foreach ($people as $person) {
+      $name = $person->name;
+      $mail = $person->mail;
+      $role = $person->role;
+      $this->taskExecStack()
+        ->stopOnFail()
+        ->exec("dktl drush user:create $name --password=$name --mail=$mail")
+        ->exec("dktl drush user-add-role $role $name")
+        ->run();
     }
+  }
 
-    /**
-     * Get user.
-     */
-    protected function getUser($name)
-    {
-        if ($this->taskExecStack()->stopOnFail()->exec("dktl drush user:information $name")->run()->wasSuccessful()
-        ) {
-            return true;
-        } else {
-            return false;
-        }
+  /**
+   * Get user.
+   */
+  protected function userExists($name) {
+    if ($this->taskExecStack()->stopOnFail()->exec("dktl drush user:information $name")->run()->wasSuccessful()) {
+      return TRUE;
     }
+    else {
+      return FALSE;
+    }
+  }
 
-    /**
-     * Get user list.
-     */
-    protected function getUsers()
-    {
-        $dktlRoot = Util::getDktlDirectory();
-        $list = file_exists("testUsers.json") ? "testUsers.json" : $dktlRoot . '/testUsers.json';
-        $json = file_get_contents($list);
-        $user = json_decode($json);
-        return $user;
-    }
+  /**
+   * Get user list.
+   */
+  protected function getUsers() {
+    $dktlRoot = Util::getDktlDirectory();
+    $list = file_exists("testUsers.json") ? "testUsers.json" : $dktlRoot . '/testUsers.json';
+    $json = file_get_contents($list);
+    $user = json_decode($json);
+    return $user;
+  }
 
-    /**
-     * Protected delete user.
-     */
-    public function deleteTestUsers()
-    {
-        $people = $this->getUsers();
-        foreach ($people as $person) {
-            $name = $person->name;
-            $user = $this->getUser($name);
-            if ($user) {
-                $this->taskExecStack()
-                    ->stopOnFail()
-                    ->exec("dktl drush user:cancel --delete-content $name -y")
-                    ->run();
-            }
-        }
+  /**
+   * Protected delete user.
+   */
+  public function deleteTestUsers() {
+    $people = $this->getUsers();
+    foreach ($people as $person) {
+      $name = $person->name;
+      $user = $this->userExists($name);
+      if ($user) {
+        $this->taskExecStack()
+          ->stopOnFail()
+          ->exec("dktl drush user:cancel --delete-content $name -y")
+          ->run();
+      }
     }
+  }
+
 }
